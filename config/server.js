a6ba4f40cce27f4782c4b4a19d620c371b28eca2
RyotaIhara <ihara1317@yahoo.co.jp>
2022-07-23 19:49:41 +0900

    by.ihara.ryota

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ed423b175cab2756c301bc7ff0735e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb019e4c-5f8b-4510-b530-89517beefca3'
+};
